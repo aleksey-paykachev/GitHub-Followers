f4a33a434ac26c6aa61dd5d7f8e9125df4042faa
Aleksey Paykachev <aleksey.paykachev@gmail.com>
2020-02-05 03:27:23 +0300

    Add subviews to SearchViewController's view.

--- GitHub-Followers/ViewControllers/SearchViewController.swift
+++ GitHub-Followers/ViewControllers/SearchViewController.swift
@@ -13,17 +13,60 @@ class SearchViewController: UIViewController {
 	init() {
 		super.init(nibName: nil, bundle: nil)
 		
-		title = "Search"
-		tabBarItem.image = UIImage(systemName: "magnifyingglass")
+		setupTabBar()
+		setupView()
+		setupSubviews()
 	}
 	
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
+
 	
-	override func viewDidLoad() {
-		super.viewDidLoad()
+	// MARK: - Setup
+	
+	private func setupTabBar() {
+		tabBarItem.title = "Search"
+		tabBarItem.image = UIImage(systemName: "magnifyingglass")
+	}
+	
+	private func setupView() {
+		view.backgroundColor = .systemBackground
+	}
+
+	private func setupSubviews() {
+		// logo
+		let logoImageView = UIImageView(image: UIImage(named: "gh-logo"))
+		logoImageView.contentMode = .scaleAspectFit
+		logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor, multiplier: 0.8).isActive = true
+
+		// text field
+		let searchTermTextField = UITextField()
+		searchTermTextField.placeholder = "Enter github username"
+		searchTermTextField.layer.borderWidth = 2
+		searchTermTextField.layer.borderColor = UIColor.tertiarySystemFill.cgColor
+		searchTermTextField.layer.cornerRadius = 8
+		searchTermTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
 		
-		view.backgroundColor = .green
+		// button
+		let searchButton = UIButton(type: .system)
+		searchButton.backgroundColor = .systemGreen
+		searchButton.tintColor = .systemBackground
+		searchButton.titleLabel?.font = .preferredFont(forTextStyle: .title3)
+		searchButton.setTitle("Find followers", for: .normal)
+		searchButton.layer.cornerRadius = 8
+		searchButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
+		
+		// stack
+		let stack = UIStackView(arrangedSubviews: [logoImageView, searchTermTextField, searchButton])
+		stack.axis = .vertical
+		stack.spacing = 10
+		stack.setCustomSpacing(40, after: logoImageView)
+
+		view.addSubview(stack)
+		stack.translatesAutoresizingMaskIntoConstraints = false
+		stack.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
+		stack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+		stack.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
 	}
 }
