b6ce99177d21bbdbf9b20904eaa3444bbb4a121d
Aleksey Paykachev <aleksey.paykachev@gmail.com>
2020-02-21 06:32:48 +0300

    Add custom GFErrorMessageView class to show error messages.

---
+++ GitHub-Followers/GF Custom UI/GFErrorMessageView.swift
@@ -0,0 +1,50 @@
+//
+//  GFErrorMessageView.swift
+//  GitHub-Followers
+//
+//  Created by Aleksey on 21.02.2020.
+//  Copyright © 2020 Aleksey Paykachev. All rights reserved.
+//
+
+import UIKit
+
+class GFErrorMessageView: UIView {
+	// MARK: - Init
+	
+	private let message: String
+	
+	init(message: String) {
+		self.message = message
+		super.init(frame: .zero)
+
+		setupView()
+		setupSubviews()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	
+	// MARK: - Setup
+	
+	private func setupView() {
+		isOpaque = false
+		
+		layer.setCornerRadius(12)
+	}
+	
+	private func setupSubviews() {
+		// blur background
+		let blurEffect = UIBlurEffect(style: .systemMaterialDark)
+		let blurEffectView = UIVisualEffectView(effect: blurEffect)
+		addSubview(blurEffectView)
+		blurEffectView.constrainToSuperview()
+		
+		// message label
+		let messageLabel = GFLabel(text: message, font: .systemFont(ofSize: 18), color: .gfTextInverted, alignment: .center, allowMultipleLines: true)
+		
+		addSubview(messageLabel)
+		messageLabel.constrainToSuperview(padding: 18)
+	}
+}
