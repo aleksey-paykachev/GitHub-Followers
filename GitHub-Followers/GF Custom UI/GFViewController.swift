9544a9eb67a39ea45ee7227a7c8d7534f295cb69
Aleksey Paykachev <aleksey.paykachev@gmail.com>
2020-02-21 10:14:56 +0300

    Add showError() method to GFViewController to show error message.

--- GitHub-Followers/GF Custom UI/GFViewController.swift
+++ GitHub-Followers/GF Custom UI/GFViewController.swift
@@ -22,6 +22,48 @@ class GFViewController: UIViewController {
 	}
 	
 	
+	// MARK: - API
+
+	func showError(_ message: String, hideAfter interval: TimeInterval = 3) {
+		let errorMessageView = GFErrorMessageView(message: message)
+		view.addSubview(errorMessageView)
+
+		// constraints
+		errorMessageView.translatesAutoresizingMaskIntoConstraints = false
+
+		let errorMessageViewTopConstraint = errorMessageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -100)
+
+		NSLayoutConstraint.activate([
+			errorMessageView.widthAnchor.constraint(equalToConstant: 300),
+			errorMessageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			errorMessageViewTopConstraint
+		])
+		
+		// prepare
+		errorMessageView.alpha = 0
+		view.layoutIfNeeded()
+		
+		// show
+		UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
+			errorMessageViewTopConstraint.constant = 10
+			errorMessageView.alpha = 1
+			self.view.layoutIfNeeded()
+		}, completion: { _ in
+			
+			// hide
+			UIView.animate(withDuration: 0.3, delay: interval, options: .curveEaseIn, animations: {
+				errorMessageViewTopConstraint.constant = -100
+				errorMessageView.alpha = 0
+				self.view.layoutIfNeeded()
+			}, completion: { _ in
+				
+				// remove
+				errorMessageView.removeFromSuperview()
+			})
+		})
+	}
+	
+	
 	// MARK: - Methods
 	
 	private func showLoadingIndicator() {
